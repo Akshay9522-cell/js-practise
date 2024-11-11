04b4e638949affc9f498955cbc7add9b1def51ac
Akshay9522-cell <akshaynamdev963@gmail.com>
2024-11-11 21:57:40 +0530

    functions

---
+++ day1.js
@@ -0,0 +1,76 @@
+ 
+ function myfunction(){
+    document.getElementById('showdate').innerText= Date();
+    
+ }
+
+ function mybutton(){
+   document.getElementById('heading').style.fontSize='75px'
+ }
+
+ function decreasesize(){
+    document.getElementById('heading').style.fontSize='30px'
+   
+ }
+
+ function mydisplayhide(){
+    document.getElementById('hide').style.display='none';
+ }
+ function mydisplay(){
+    document.getElementById('hide').style.display='block'
+ }
+ function mycolor(){
+    document.getElementById('box').style.backgroundColor='red'
+ }
+
+ function mycolor1(){
+    document.getElementById('box').style.backgroundColor='blue'
+ }
+
+ function mypara(){
+     document.getElementById('para').innerHTML='my pargraph is changed hurreee'
+ }
+
+    //window.alert('just see my functions')
+ 
+    let carname='Volvo',price= 30000
+    document.getElementById('car').innerHTML=carname
+    document.getElementById('car').innerHTML=price
+
+    let x
+    x=  "volvo"
+    x=30  
+    console.log(x)
+
+    let y= "volvo" + 20+20
+    console.log(y)
+
+
+
+
+
+// let button=document.getElementById('btn')
+
+// console.log(button)
+
+
+// button.onclick=()=> console.log(new Date());
+
+// document.getElementById('btn').innerText=button
+
+
+// const cybrome=(name,batch,course)=>{
+
+//     const student={
+//           name:"name",
+//           batch:"batch",
+//           course:"course"
+
+//     }
+//     let data=` my name is ${name} i am from batch no ${batch} and i am doing ${course}`
+//     return data
+
+// }
+//  console.log(cybrome("manu sharma","P39","Mern Stack "))
+//  console.log(cybrome("Ayush","P39","java developer") )
+
